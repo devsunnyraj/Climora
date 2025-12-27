55b376fcfbe4943ea048c30debb3c9bb26650790
Sunny Raj <hellotheresunnyhere@gmail.com>
2025-12-27 17:58:18 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -9,10 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-  }),
-];
+const eslintConfig = compat.config({
+  extends: ["next/core-web-vitals", "next/typescript"],
+});
 
 export default eslintConfig;
